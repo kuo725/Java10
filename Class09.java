6d2e0e22dd63eae46f781f740db25f17c50bc9a0
kuo725 <113486769+kuo725@users.noreply.github.com>
2022-10-30 14:52:49 +0800

    Create Class09.java

---
+++ Class09.java
@@ -0,0 +1,20 @@
+class Caaa3 {
+	private int num;
+
+	public void display() {
+		System.out.println("printed from Caaa class");
+	}
+}
+
+class Cbbb3 extends Caaa3 {
+	public void display() {
+		System.out.println("printed from Cbbb class");
+	}
+}
+
+public class Class09 {
+	public static void main(String args[]) {
+		Cbbb3 bb = new Cbbb3();
+		bb.display();
+	}
+}
